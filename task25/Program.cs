e7044b77d317cddd8345569ba34290449c9b4ef6
pavelkulak <kulak-pavel@yandex.ru>
2023-07-16 14:17:40 +0300

    123

--- task25/Program.cs
+++ task25/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.WriteLine("Введите число A:");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите степень B:");
+int B = int.Parse(Console.ReadLine());
+
+int result = 1;
+for (int i = 0; i < B; i++)
+{
+    result *= A;
+}
+
+Console.WriteLine($"Результат: {result}");
---
+++ task27/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+
+int sum = 0;
+int originalNumber = number;
+
+while (number > 0)
+{
+    int digit = number % 10;
+    sum += digit;
+    number /= 10;
+}
+
+Console.WriteLine($"Сумма цифр числа {originalNumber}: {sum}");
+
